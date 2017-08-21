fe1322200032c8fac91399fac56c45ff28aaaf65
Jason <jlukose12@gmail.com>
2017-08-21 11:06:35 -0400

    Hello World C++, compiled with g++

---
+++ ClassNotes/SimplePrograms/myFirst.cpp
@@ -0,0 +1,28 @@
+// Comments in C++ are created with two slashes
+// In-line comment 
+/*
+Multi Line Comment
+*/
+
+// Hello World Program
+// First we include preprocessor directives
+// # <preprocessor directive>
+// This include tells preprocessor to pull in existing libs
+
+// iostream lib contains Input/Output commands
+#include <iostream>
+
+// Every program must have one main function
+
+int main()
+{
+
+// The body of the main function is contained in curly braces
+// This function is named main, it returns an integer and takes no args
+
+std::cout << "Hello, this is my first C++ Program. \n";
+// Streaming string to standard output
+
+}
+
+
