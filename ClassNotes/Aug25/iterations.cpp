554792db8784c6d7fa559881ce038531e27b3ba6
Jason <jlukose12@gmail.com>
2017-08-25 10:52:51 -0400

    ClassNotes only

---
+++ ClassNotes/Aug25/iterations.cpp
@@ -0,0 +1,26 @@
+// Iterative tools!
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+double n = 1.0;
+
+while (n < 11.0) {
+
+    cout << n << endl;
+
+    n++;
+
+
+}
+
+
+
+
+return 0;
+
+
+
+}
---
+++ ClassNotes/Aug25/moreIterations.cpp
@@ -0,0 +1,14 @@
+// More iterations
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    double num = 0;
+    int counter = 0;
+
+
+
+
+}
